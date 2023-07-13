6071964e64e67dbaf9b9c05dd0b740276eee10b1
Jim Buitt <jbuitt@gmail.com>
2023-07-12 21:21:13 -0500

    Added typo

--- app/Jobs/ProcessEmwinZipFileJob.php
+++ app/Jobs/ProcessEmwinZipFileJob.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use App\Events\NewProductArrived;
