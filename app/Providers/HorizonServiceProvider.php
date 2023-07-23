4390efa5fc295afd36959489524eb4293a746012
Jim Buitt <jbuitt@gmail.com>
2023-07-23 09:07:29 -0500

    Adding admin email config var and using it for Horizon and Telescope access in production env

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -29,7 +29,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user) {
             return in_array($user->email, [
-                //
+                config('emwin-controller.admin_email')
             ]);
         });
     }
