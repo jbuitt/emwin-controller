89868ce934b6ff919c1e33e18a65ab2747706c58
Jim Buitt <jbuitt@gmail.com>
2023-08-08 17:21:15 -0500

    Fixing PSR-4 problem with Syslog model

--- app/Models/Syslog.php
+++ app/Models/Syslog.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SysLog extends Model
+class Syslog extends Model
 {
     use HasFactory;
 
